9b38cc0506cf0c7d45950fe75c68082c63901faa
cbrundige <chrisbrundige@protonmail.com>
2020-08-09 12:35:52 -0400

    Initial Commit

---
+++ C867 Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867 Project
+//
+//  Created by Chris Brundige on 8/9/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
